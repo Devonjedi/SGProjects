592bb628cc49ff4e9a260409e7b1bba1c2d22eca
Devonjedi <devon.jedi@gmail.com>
2021-01-07 22:09:42 -0600

    clickToAddItems JS exercise

--- JSPractice/Old Practice/defFunc.js
+++ JSPractice/Old Practice/defFunc.js
@@ -1,10 +1,7 @@
-/*
+
 function addTwoNumbers(firstNumber, secondNumber) {
     
     // Add code to function in the function body to return the sum of firstNumber and secondNumber.
-    var firstNumber = parseInt(prompt("Enter the first number"));
-
-    var secondNumber = parseInt(prompt("Enter the second number"));
 
     //Add two numbers
     addTwoNumbers = firstNumber + secondNumber;
@@ -12,30 +9,6 @@ function addTwoNumbers(firstNumber, secondNumber) {
     //Display the sum
     console.log(`The sum of ${firstNumber} and ${secondNumber} is ${addTwoNumbers}`);
 
+    return addTwoNumbers;
 }
-*/
 
-var friends = ["Aneki", "Quell", "Clarity", "Sleepy", "Roghar", "DM Crimson"];
-var team1 = new Array();
-var team2 = new Array();
-
-/*
-for (var i = 0; i < friends.length; i++) {
-    if (i % 2 == 0) {
-        // if the index is even, add the person to team 1 
-        team1.push(friends[i]);
-    } else {
-        // otherwise add them to team 2
-        team2.push(friends[i]);
-    }
-}
-*/
-
-for (var i = 0; i < friends.length; i++) {
-    if (i % 2 == 0) {
-        // if the index is even, add the person to team 1
-        team1.unshift(friends[i]);
-    } else {
-        team2.unshift(friends[i]);
-    }
-}
